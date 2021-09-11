9d02c2e279b6162b6b8a603d1fa7456ef509c868
amit-sec <66508064+amit-sec@users.noreply.github.com>
2021-09-12 02:56:50 +0530

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,1 +1,44 @@
+#!/usr/bin/env python
+
+#coded during zaid sabih course on udemy
+import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", dest = "ipaddr" , help = " add ip address")
+    (options, arguments) = parser.parse_args()
+    if not options.ipaddr:
+        parser.error("please specify ip address")
+    return options
+     
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
+     
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+def print_result(results_list):
+    
+    print("IP\t\t\tMac Address\n------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+        
+options = get_arguments()
+scan_result = scan(options.ipaddr)
+print_result(scan_result)
+
+
+
+
+
+
+
 
